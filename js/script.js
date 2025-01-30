295973868b6a8810d73874dadd5deb0b0c2e5c5b
Yudhapra <yudhapra23@gmail.com>
2025-01-30 20:01:21 +0700

    final update

--- js/script.js
+++ js/script.js
@@ -32,30 +32,27 @@ function checkBMI() {
     } else if (beratBadan > 0 && tinggiBadan > 0 && usia > 0) {
         
         // Menentukan kategori BMI berdasarkan nilai BMI yang sudah dihitung
-        if (bmi < 18.5) {
+        if (bmi < 18.45) {
         bmiCategory = 'Kurus';
         bmiCategory1 = 'Berat badan kurang';
         rangeCategory = 'Hasil BMI < 18.5';
         descCategory = 'Anda berada dalam kategori kurus atau berat badan kurang. Cara terbaik untuk menaikkan berat badan adalah dengan mengatur kalori makanan yang dikonsumsi dan berolahraga.<br>Jika BMI Anda berada dalam kategori ini maka Anda dianjurkan untuk menaikkan berat badan hingga batas normal.';
         diseaseDesc = 'Beberapa penyakit yang berasal dari berat badan kurang antara lain:<br><br>';
         diseaseCategory = '<ul>Osteoporosis</ul><ul>Menstruasi tidak teratur</ul><ul>Menurunnya sistem kekebalan tubuh</ul><ul>Menurunnya massa otot</ul>';
-
-        } else if (bmi >= 18.5 && bmi < 24.9) {
+        } else if (bmi >= 18.45 && bmi <= 24.99) {
         bmiCategory = 'Normal';
         bmiCategory1 = 'Berat badan normal';
         rangeCategory = 'Hasil BMI diantara 18.5 dan 24.9';
         descCategory = 'Anda berada dalam kategori normal atau berat badan ideal. Cara terbaik untuk menjaga berat badan adalah dengan mengatur pola makan dan berolahraga secara teratur.';
         diseaseDesc = 'Beberapa keuntungan yang berasal dari berat badan normal antara lain:<br><br>';
         diseaseCategory = '<ul>Resiko penyakit jantung menurun</ul><ul>Resiko diabetes menurun</ul><ul>Resiko stroke menurun</ul><ul>Resiko kanker menurun</ul>';
-
-        } else if (bmi >= 25 && bmi < 29.9) {
+        } else if (bmi >= 25 && bmi <= 29.99) {
         bmiCategory = 'Kegemukan';
         bmiCategory1 = 'Berat badan berlebih';
         rangeCategory = 'Hasil BMI diantara 25 dan 29.9';
         descCategory = 'Anda berada dalam kategori Kegemukan atau berat badan berlebih. Cara terbaik untuk menurunkan berat badan adalah dengan mengatur kalori makanan yang dikonsumsi dan berolahraga.<br>Jika BMI Anda berada dalam kategori ini maka Anda dianjurkan untuk menurunkan berat badan hingga batas normal.';
         diseaseDesc = 'Beberapa penyakit yang berasal dari kegemukan antara lain:<br><br>';
         diseaseCategory = '<ul>Diabetes</ul><ul>Hipertensi</ul><ul>Sakit Jantung</ul><ul>Osteoarthritis</ul>';
-
         } else {
         bmiCategory = 'Obesitas';
         bmiCategory1 = 'Berat badan obesitas';
@@ -69,7 +66,7 @@ function checkBMI() {
     // Untuk menampilkan hasil BMI, status, saran, dan risiko penyakit
     document.getElementById('result-bmi').textContent = bmi.toFixed(1);
     document.getElementById('bmi-category1').textContent = bmiCategory1;
-    document.getElementById('bmi-category2').textContent = `Anda ${gender} usia ${usia} tahun yang memiliki berat badan ${bmiCategory}`;
+    document.getElementById('bmi-category2').textContent = `Anda ${gender} berusia ${usia} tahun yang memiliki berat badan ${bmiCategory}`;
     document.getElementById('range').textContent = rangeCategory;
     document.getElementById('desc').innerHTML = descCategory;
     document.getElementById('disease1').innerHTML = diseaseDesc;
